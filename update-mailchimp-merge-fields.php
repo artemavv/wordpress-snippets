a9bbe0af67a5b73a083898e1db63c3f60fa26cd6
artem <artem.the.freelancer@gmail.com>
2024-06-12 19:50:22 +0400

    Code to update contact reward points in MailChimp: added hook caller, improved exception handling

--- update-mailchimp-merge-fields.php
+++ update-mailchimp-merge-fields.php
@@ -1,39 +1,38 @@
 <?php
 
-function update_reward_points_in_mailchimp( $user_id, $new_reward_points ) {
-		
-    $is_subscribed = get_user_meta( $user_id, 'mailchimp_woocommerce_is_subscribed', true );
 
-    $list_id = '0abcde98765';
-
-    $hash = md5( strtolower( trim( $email ) ) );
+add_action( 'woocommerce_order_status_completed', 'update_reward_points_in_mailchimp' );
 
 
-    if ( $is_subscribed ) {
-        $data = array( 'merge_fields' => array( 'MMERGE4' => $new_reward_points ) );
+function update_reward_points_in_mailchimp( $order_id ) {
 
-        mailchimp_debug( 'api.update_member', "Updating {$email}", $data );
+    $order = wc_get_order( $order_id );
 
-        try {
-            return $this->put( "lists/$list_id/members/$hash?skip_merge_validation=true", $data );
-        } catch ( Exception $e ) {
+    if ( is_object( $order) ) {
 
-            if ( $data['status'] !== 'subscribed' || ! mailchimp_string_contains( $e->getMessage(), 'compliance state' ) ) {
+        $user_id = $order->get_user_id();
+        $new_reward_points = (float) get_user_meta( $user_id, '_reward_points', true );
+        $is_subscribed = get_user_meta( $user_id, 'mailchimp_woocommerce_is_subscribed', true );
 
+        $list_id = '0abcde98765';
+
+        $hash = md5( strtolower( trim( $email ) ) );
+
+        if ( $is_subscribed ) {
+            $data = array( 'merge_fields' => array( 'MMERGE4' => $new_reward_points ) );
+
+            mailchimp_debug( 'api.update_member', "Updating {$email}", $data );
+
+            try {
+                $result = $this->put( "lists/$list_id/members/$hash?skip_merge_validation=true", $data );
+
+                mailchimp_debug( 'api.update_member', "Get results with {$email} when updating", $result );		
+            } catch ( Exception $e ) {
                 mailchimp_debug( 'api.update_member', "Got an exception with {$email} when updating", $e );		
-                throw $e;
             }
-
-            $data['status'] = 'pending';
-            $result         = $this->patch( "lists/$list_id/members/$hash?skip_merge_validation=true", $data );
-            mailchimp_log( 'api', "{$email} was in compliance state, sending the double opt in message" );
-            return $result;
-
         }
-
-    }
-    else {
-        mailchimp_debug( 'api.update_member', "Will not update {$email} because they are not subscribed", $data );
-    }
-    
+        else {
+            mailchimp_debug( 'api.update_member', "Will not update {$email} because they are not subscribed", $data );
+        }
+    }       
 }
